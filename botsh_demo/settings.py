b805bb5095c88da95a970fb6cfce319c133c4c89
mvamsee <mvk.kiran@outlook.com>
2022-09-27 21:24:15 +0530

    allowed all hosts

--- botsh_demo/settings.py
+++ botsh_demo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-^=)8qu-dk1)s2bcao!ielj^htnd0y6cm%r(w0bl(_bhqrfu9#a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
